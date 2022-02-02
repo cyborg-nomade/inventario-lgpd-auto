16e540fb045937c5a36cf26a56f65a510c536daa
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-02 11:11:43 -0300

    pequenas configurações

--- inventario-lgpd-frontend/src/App.js
+++ inventario-lgpd-frontend/src/App.js
@@ -1,14 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import Button from "react-bootstrap/Button";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Button>Hi</Button>
     </div>
   );
-}
+};
 
 export default App;
