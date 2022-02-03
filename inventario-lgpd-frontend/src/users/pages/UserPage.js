2f89ec5675d1e55d97335b02c1423e0fac1b2546
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-03 19:03:33 -0300

    teste inicial da navbar

--- inventario-lgpd-frontend/src/users/pages/UserPage.js
+++ inventario-lgpd-frontend/src/users/pages/UserPage.js
@@ -1,10 +1,11 @@
 import React from "react";
-import CasesList from "./../../shared/components/CasesList";
 import { Outlet } from "react-router-dom";
+import MainHeader from "../../shared/components/nav/MainHeader";
 
 const UserPage = () => {
   return (
     <React.Fragment>
+      <MainHeader />
       <Outlet />
     </React.Fragment>
   );
