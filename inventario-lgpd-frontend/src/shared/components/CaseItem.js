c7700e8137db5075d3290188cb87262561e65614
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-03 16:46:34 -0300

    instalado propTypes + definido tipo CaseItemObject + elaborado componentes da lista e do item

--- inventario-lgpd-frontend/src/shared/components/CaseItem.js
+++ inventario-lgpd-frontend/src/shared/components/CaseItem.js
@@ -1,7 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+/**
+ *
+ *
+ * @param {{item:CaseItemObject}} props
+ *
+ */
 const CaseItem = (props) => {
-  return <div>it's a case item</div>;
+  return <tr>
+  <td>{props.item.nome}</td>
+  <td>{props.item.id}</td>
+  <td>{props.item.area}</td>
+  <td>{props.item.dataCriacao}</td>
+  <td>{props.item.dataAtualizacao}</td>
+  <td>{props.item.finalidade}</td>
+  <td>{props.item.hipoteseTratamento}</td>
+  <td>{props.item.dadosPessoaisSensiveis}</td>
+</tr>;
+};
+
+CaseItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.number,
+    area: PropTypes.string,
+    dataCriacao: PropTypes.string,
+    dataAtualizacao: PropTypes.string,
+    finalidade: PropTypes.string,
+    hipoteseTratamento: PropTypes.string,
+    dadosPessoaisSensiveis: PropTypes.bool,
+  }),
 };
 
 export default CaseItem;
--- inventario-lgpd-frontend/src/shared/components/CasesList.js
+++ inventario-lgpd-frontend/src/shared/components/CasesList.js
@@ -1,7 +1,66 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+
+import CaseItem from "./CaseItem";
+
+/**
+ *
+ *
+ * @param {{items:CaseItemObject[]}} props
+ *
+ */
 const CasesList = (props) => {
-  return <div>a case list!</div>;
+  if (props.items.length === 0) {
+    return (
+      <Alert variant="warning">
+        <Alert.Heading>Nada foi encontrado</Alert.Heading>
+        <p>
+          Não foi encontrado nenhum item do inventário de casos de uso de dados
+          pessoais com o filtro determinado.
+        </p>
+      </Alert>
+    );
+  }
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>ID</th>
+          <th>Área</th>
+          <th>Data de Criação do Inventário</th>
+          <th>Data de Atualização do Inventário</th>
+          <th>Finalidade</th>
+          <th>Hipotese de Tratamento</th>
+          <th>Trata Dados Pessoais Sensíveis?</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.items.map((item) => (
+          <CaseItem key={item.id} item={item} />
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+CasesList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.number,
+      area: PropTypes.string,
+      dataCriacao: PropTypes.string,
+      dataAtualizacao: PropTypes.string,
+      finalidade: PropTypes.string,
+      hipoteseTratamento: PropTypes.string,
+      dadosPessoaisSensiveis: PropTypes.bool,
+    })
+  ),
 };
 
 export default CasesList;
