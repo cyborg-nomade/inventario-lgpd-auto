65deb6cfbf94f4b574e531e79379a1e856f4dc7f
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-04 12:22:06 -0300

    outsourced navlinks to its own component + styles

--- inventario-lgpd-frontend/src/shared/components/nav/MainHeader.js
+++ inventario-lgpd-frontend/src/shared/components/nav/MainHeader.js
@@ -2,9 +2,9 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const MainHeader = () => {
   return (
@@ -16,21 +16,11 @@ const MainHeader = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link eventKey={1} as={NavLink} to="/">
-                Página Inicial
-              </Nav.Link>
-              <Nav.Link eventKey={2} as={NavLink} to="/cases/new">
-                Novo
-              </Nav.Link>
-              <Nav.Link eventKey={3} as={NavLink} to="/logout">
-                Sair
-              </Nav.Link>
-              <NavDropdown title="Ações do Comitê" id="offcanvasNavbarDropdown">
-                <NavDropdown.Item eventKey={4} as={NavLink} to="/cases/approve">
-                  Aprovações Pendentes
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav
+              className="justify-content-end flex-grow-1 pe-3"
+              variant="pills"
+            >
+              <NavLinks />
             </Nav>
           </Navbar.Collapse>
         </Container>
