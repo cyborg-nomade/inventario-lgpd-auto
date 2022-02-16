9f46594dfba95100aaf2afb1315fca6c3a3bff3e
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-16 12:55:40 -0300

    object alterations before posting new

--- inventario-lgpd-frontend/src/cases/pages/NewCase.tsx
+++ inventario-lgpd-frontend/src/cases/pages/NewCase.tsx
@@ -1,14 +1,31 @@
 import CaseForm from "../components/CaseForm";
 import { CONNSTR } from "./../../App";
+import { USERS } from "./AllCasesList";
 import {
   emptyFullCaseObject,
   FullCaseObject,
 } from "./../../shared/models/cases.model";
+import { useNavigate, useParams } from "react-router-dom";
+
+let idCounter = 0;
 
 const NewCase = () => {
+  const uid = useParams().uid || 101;
+  let navigate = useNavigate();
+
   const submitFormHandler = async (item: FullCaseObject) => {
     console.log(item);
-    console.log(CONNSTR);
+
+    item.id = (idCounter++).toString();
+    item.area = item.extensaoEncarregado.area || "";
+    item.criador = USERS[+uid - 1];
+    for (const value of Object.values(item.categoriaDadosPessoaisSensiveis)) {
+      if (value.descricao !== "Não se aplica") {
+        item.dadosPessoaisSensiveis = true;
+      }
+    }
+
+    console.log(item);
 
     const response = await fetch(`${CONNSTR}cases.json`, {
       method: "POST",
@@ -20,6 +37,7 @@ const NewCase = () => {
 
     const data = await response.json();
     console.log(data);
+    navigate(`/`);
   };
 
   const emptyItem = emptyFullCaseObject();
