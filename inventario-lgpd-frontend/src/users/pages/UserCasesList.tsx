d471706120c76b7b780ec1e730d92aa276e4ea90
Uriel Fiori <uriel.fiori@gmail.com>
2022-02-16 15:09:38 -0300

    user page list new fecthes from firebase

--- inventario-lgpd-frontend/src/users/pages/UserCasesList.tsx
+++ inventario-lgpd-frontend/src/users/pages/UserCasesList.tsx
@@ -1,10 +1,70 @@
-import CasesList from "../../cases/components/CasesList";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { CASES } from "../../cases/pages/AllCasesList";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import Row from "react-bootstrap/Row";
+
+import { CONNSTR } from "../../App";
+import {
+  CaseItemObject,
+  reduceCaseObject,
+} from "../../shared/models/cases.model";
+import CasesList from "../../cases/components/CasesList";
 
 const UserCasesList = () => {
   const uid = useParams().uid;
-  const filteredCases = CASES.filter((item) => item.criador.userCode === uid);
+
+  const [cases, setCases] = useState<CaseItemObject[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getAllCases = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      const response = await fetch(`${CONNSTR}cases.json`);
+      if (!response.ok) {
+        throw new Error("Algo deu errado!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedCases: CaseItemObject[] = [];
+
+      for (const key in responseData) {
+        loadedCases.push(reduceCaseObject(responseData[key]));
+      }
+
+      setCases(loadedCases);
+      setIsLoading(false);
+    };
+
+    getAllCases().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Row className="justify-content-center">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Row>
+    );
+  }
+
+  if (error) {
+    return (
+      <Row className="justify-content-center">
+        <Alert variant="danger">{error}</Alert>
+      </Row>
+    );
+  }
+
+  const filteredCases = cases.filter((item) => item.criador.userCode === uid);
 
   return <CasesList items={filteredCases} />;
 };
